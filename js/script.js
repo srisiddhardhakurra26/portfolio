5c651ed1c37237501490234c7bee3c56db74b796
siddhardha <sk2943@njit.edu>
2024-05-06 16:27:12 -0400

    portfolio page

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Add your custom JavaScript code here
+
+// Example: Smooth scrolling navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
